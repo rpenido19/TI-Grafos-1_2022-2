e8c21404f9d10d95b7aca42083b658e7cbb22838
Rafael Penido Rocha <72994393+rpenido19@users.noreply.github.com>
2022-06-12 14:09:30 -0300

    Primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("   _____              ____        \r\n" + "  / ___/__________ __/ __/_  _____\r\n"
+		+ " / / _ / ___/ __  /\\  / _  \\/ ___/\r\n" + "/ /_/ / /  / /_/ / / / /_/ /__  ) \r\n"
+		+ "\\____/_/   \\_____\\/_/\\____/____/  \r\n" + "\r\n");
+		System.out.println("Pontificia Universidade Catolica De Minas Gerais - Sistemas de Informacao - 2022");
+		System.out.println("Gustavo Henrique, Matheus Ryuji, Maxwuell Junio, Rafael Penido" + "\r");
+
+		Reader reader = new Reader();
+		System.out.println(reader.readFile());
+	}
+}
---
+++ src/Reader.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Reader {
+    private final String FILENAME = "estrada.txt";
+    private final String PATH = System.getProperty("user.dir");
+
+    public String getFILENAME() {
+        return FILENAME;
+    }
+
+    public String getPATH() {
+        return PATH;
+    }
+
+    public ArrayList<String> readFile() {
+        ArrayList<String> data = new ArrayList<String>();
+        try {
+            File file = new File(getPATH() + "\\" + getFILENAME());
+            Scanner input = new Scanner(file);
+            while (input.hasNextLine()) {
+                data.add(input.nextLine());
+            }
+            input.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Ocorreu um erro! \"" + getFILENAME() + "\" não encontrado.");
+        }
+        return data;
+    }
+}
